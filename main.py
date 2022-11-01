3b8be220dcaa0b894bfedeaceffab8e2d7a00369
kanggeonnim <gun4329@gmail.com>
2022-11-01 21:35:38 +0900

    func exit added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # Program make a simple calculator
+from contextlib import nullcontext
 import logging
 import sys
 import inspect
 
 from calculate import add, subtract, multiply, divide
+from exit import isExit
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -13,15 +15,11 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 
-yes_list = ['yes', 'y']
-no_list = ['no', 'n']
-
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide") 
-
 
 while True:
     # take input from the user
@@ -30,6 +28,8 @@ while True:
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
         try:
+            num1 = None # 숫자가 아닌 값이 들어갈 경우 except에 의해 이전값이 사용될 수 있으니 초기화.
+            num2 = None
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
             logging.info(f'INFO: input numbers are {num1} and {num2}')
@@ -52,35 +52,10 @@ while True:
                 print("Not divisible by zero")
                 logger.error(f'Error: {e}')
 
-        # check if user wants another calculation
-        # break the while loop if answer is no
-
-        next_calculation = (input("Let's do next calculation? (yes/no): ")).lower()
-        while True:
-            
-            if next_calculation in yes_list:    # 계산을 더 하고 싶을 때
-                break
-            elif next_calculation in no_list:   # 더이상 계산을 하지 않을 때
-                exit_answer = (input("Are you sure? (yes/no): ")).lower()   # 한번 더 물어봄
-
-                while True: # yes 또는 no로만 대답을 받을 때 까지 질문함.
-
-                    if exit_answer in yes_list or exit_answer in no_list:
-                        break
-                    else:   # 선택지에 없는 입력을 받았을 경우
-                        exit_answer = (input("Quit the calculator? Please answer only (yes/no): ")).lower()
-
-                if exit_answer in no_list: # 사실 속마음은 프로그램 종료를 원하지 않을 경우
-                    next_calculation = "yes"
-                elif exit_answer in yes_list: # 정말로 프로그램 종료를 원하는 경우
-                    break
-
-            else:   # 선택지에 없는 입력을 받았을 경우
-                next_calculation = (input("Calculation again? Please answer only (yes/no): ")).lower()
-
-        if next_calculation in no_list:
+        next_calculation = isExit()
+        if next_calculation == "no":
             break
-        elif next_calculation in yes_list:
+        elif next_calculation == "yes":
             continue
 
 
